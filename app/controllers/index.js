5709a5f3c96db6f7a1940a01548d773ef52ab474
palmierie <ericmpalmieri@gmail.com>
2017-11-20 18:15:53 -0600

    prep for deploy

---
+++ app/controllers/index.js
@@ -0,0 +1,8 @@
+// "use strict";
+// require('./db-toggle.controller.js');
+// require('./home.controller.js');
+// require('./login-alert.controller.js');
+// require('./my-list.controller.js');
+// require('./nav.controller.js');
+// require('./search.controller.js');
+// require('./user.controller.js');
