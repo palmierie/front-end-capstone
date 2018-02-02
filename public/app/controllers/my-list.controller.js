f1109b58ce09c4be30b900e5c4e2e33f81a61c7f
palmierie <ericmpalmieri@gmail.com>
2018-02-02 14:35:50 -0600

    fixed footer

--- public/app/controllers/my-list.controller.js
+++ public/app/controllers/my-list.controller.js
@@ -17,6 +17,11 @@
         let patchObj = {};
         patchObj = userObj;
         $scope.arraySongObj = userObj.myList;
+        //display alt html if no songs are in myList
+        $scope.myListPlaylistEmpty = true;
+        if (userObj.myList.length > 0){
+          $scope.myListPlaylistEmpty = false;
+        }
         
         $scope.deleteFunction = function(event){
           //get song ID
